2acf4e82a0bfdc5795bb0f721cc8b7d17dc0f65b
Repository manager <github@actions>
2023-07-04 11:52:40 +0000

    Update binary rules file

--- output/worker_configuration.js
+++ output/worker_configuration.js
@@ -1,7 +1,6 @@
-// Configuration for the update server
-if (chrome_version >= 115) {
-  sha256 = '7fa6fb1be41a21f0db3d3baafceef3abd535e1e3bc4a90d6836f0b10987518ad';
-  sha1 = 'OXE3v4s6G7j9dqHJmCrTzkPKe+g=';
-  filesize = '653929';
-  filename = '23.185_subresource_rules.crx3';
-}
+  if (version_to_use == '23.185') {
+    sha256 = '7fa6fb1be41a21f0db3d3baafceef3abd535e1e3bc4a90d6836f0b10987518ad';
+    sha1 = 'OXE3v4s6G7j9dqHJmCrTzkPKe+g=';
+    filesize = '653929';
+    filename = '23.185_subresource_rules.crx3';
+  }
