ad108974d0d1880ce997b6a36ac58df90602a4bc
Kiwi Browser <40272275+kiwibrowser@users.noreply.github.com>
2023-07-04 13:33:56 +0300

    Create generate_sha1_base64_signature.py

---
+++ tools/generate_sha1_base64_signature.py
@@ -0,0 +1,15 @@
+import hashlib
+import base64
+import sys
+
+# Get the filename from the first command-line argument
+filename = sys.argv[1]
+
+with open(filename, 'rb') as f:
+    h = hashlib.sha1()
+    h.update(f.read())
+    hash_sha1 = h.digest()
+
+encoded_base64 = base64.b64encode(hash_sha1)
+
+print(encoded_base64)
